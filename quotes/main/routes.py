bb9eb96d6fbdfb1c1b846f312f6175e2321fd127
HanslettTheDev <HanslettTheDev@gmail.com>
2021-08-01 00:22:21 +0100

    fied mobile responsive issues

--- quotes/main/routes.py
+++ quotes/main/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, url_for
 from quotes.models import Quotes
 
 
@@ -8,5 +8,5 @@ main = Blueprint('main', __name__)
 @main.route("/home")
 def home():
     page = request.args.get('page', 1, type=int)#if the someone tries to use any other data type apart from an int it will throw an error
-    posts = Quotes.query.order_by(Quotes.date_posted.desc()).paginate(page=page, per_page=6)
+    posts = Quotes.query.order_by(Quotes.date_posted.desc()).paginate(page=page, per_page=15)
     return render_template('index.html', posts=posts)
