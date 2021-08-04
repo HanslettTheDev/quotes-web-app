8ca22302200a2df094b16222704c786f6fd503f8
HanslettTheDev <HanslettTheDev@gmail.com>
2021-08-04 23:42:01 +0100

    pre-deploy test

--- quotes/config.py
+++ quotes/config.py
@@ -1,12 +1,12 @@
 from boto.s3.connection import S3Connection
 import os
 
-s3 = S3Connection(os.environ['SECRET_KEY'], os.environ['DATABASE_URI'], os.environ['mail_username'], os.environ['mail_password'], os.environ['PORT'])
+s3 = S3Connection(os.environ['SECRET_KEY'], os.environ['DATABASE_URI'], os.environ['mail_username'], os.environ['mail_password'])
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = os.environ('PORT')
+    MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_TLS = True 
     MAIL_USERNAME = os.environ.get('mail_username')
