d32c3390bb69b88d4333523994d07e55bea2d29e
HanslettTheDev <HanslettTheDev@gmail.com>
2024-06-30 00:45:20 +0100

    fix: changed the os.environ[''] format to os.environ.get() meethod
    Signed-off-by: HanslettTheDev <HanslettTheDev@gmail.com>

--- quotes/config.py
+++ quotes/config.py
@@ -3,8 +3,8 @@ import os
 
 # s3 = S3Connection(os.environ['SECRET_KEY'], os.environ['DATABASE_URI'], os.environ['mail_username'], os.environ['mail_password'])
 class Config():
-    SECRET_KEY = os.environ['SECRET_KEY']
-    SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
